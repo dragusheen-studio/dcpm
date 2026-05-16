ffdbcfc34d64c9e68f874f0da85e4e8ac8699cec
dragusheen <nathan.tirolf@epitech.eu>
2026-05-16 11:41:34 -0400

    [+] Update cmd

--- dcpm/commands/__init__.py
+++ dcpm/commands/__init__.py
@@ -5,13 +5,14 @@ from .run import RunCommand
 from .help import HelpCommand
 from .build import BuildCommand
 from .remove import RemoveCommand
-
+from .update import UpdateCommand
 
 COMMANDS = {
     "init": InitCommand(),
     "add": AddCommand(),
 	"remove": RemoveCommand(),
     "install": InstallCommand(),
+	"update": UpdateCommand(),
     "run": RunCommand(),
 	"build": BuildCommand(),
     "help": HelpCommand()
